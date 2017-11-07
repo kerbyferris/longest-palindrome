9ea8d53ac762a1da5023fa62780da3bc43d912e3
Kerby Ferris <kerbyferris@gmail.com>
2017-11-07 08:59:18 -0800

    clojure attempt

--- clojure/palindrome/src/palindrome/core.clj
+++ clojure/palindrome/src/palindrome/core.clj
@@ -1,10 +1,69 @@
 (ns palindrome.core)
 
-(is-palindrome "abba")
-(defn is-palindrome[str]
-  str
-  )
+(defn palindrome?[str]
+  (let [half (quot (count str) 2)]
+    (= (take half str)
+       (take half (reverse str)))))
+
+(defn get-pivot[current next]
+  (cond
+    (> (count current) 1) current
+    (= current next) (str current next)
+    :else (str current)))
+
+(defn update-longest[candidate-palindrome longest-palindromes]
+  (let [longest (count (first longest-palindromes))
+        candidate-length (count candidate-palindrome)]
+    (cond
+      (< candidate-length longest) longest-palindromes
+      (> candidate-length longest) [candidate-palindrome]
+      :else (set (cons candidate-palindrome longest-palindromes)))))
+
+(defn get-next-candidate[pivot processed unprocessed]
+  (let [reach-left (dec (quot (count pivot) 2))
+        reach-right (if (even? (count pivot))
+                      (inc reach-left)
+                      reach-left)
+        maybe-start (first (drop reach-left (reverse processed)))
+        maybe-end (first (drop reach-right unprocessed))]
+    (if (and maybe-start maybe-end)
+      (str maybe-start pivot maybe-end))))
+
+(defn get-palindrome[string]
+  (loop [processed ""
+         unprocessed string
+         current (str (first unprocessed))
+         longest []]
+    (if (= "" unprocessed)
+      longest
+      (let [pivot (get-pivot current (str (first (rest unprocessed))))
+            maybe-palindrome (get-next-candidate pivot processed unprocessed)
+            next-processed (str processed (first unprocessed))
+            next-unprocessed (apply str (rest unprocessed))]
+        (do
+          (println ;"reach" reach "\t"
+                   "current: " current"\t"
+                   "proc: " processed "\t"
+                   "unproc: " unprocessed "\t"
+                   "pivot " pivot "\t"
+                   "maybe p: " maybe-palindrome)
+          (cond
+            (nil? maybe-palindrome) (recur next-processed
+                                           next-unprocessed
+                                           (str (first next-unprocessed))
+                                           longest)
+            (palindrome? maybe-palindrome) (recur processed
+                                                  unprocessed
+                                                  maybe-palindrome
+                                                  (update-longest maybe-palindrome longest))
+            :else (recur next-processed
+                         next-unprocessed
+                         (str (first next-unprocessed))
+                         (update-longest pivot longest))))))))
 
 (defn -main[]
-  (foo "blah"))
-
+  ; (println (get-palindrome "xabbablahblahracecar1234321"))
+  (println (get-palindrome "abba"))
+  ; (println (get-palindrome "aba"))
+  ; (println (get-palindrome "abaaaabaaa"))
+  )
--- clojure/palindrome/test/palindrome/core_test.clj
+++ clojure/palindrome/test/palindrome/core_test.clj
@@ -2,6 +2,25 @@
   (:require [clojure.test :refer :all]
             [palindrome.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(def palindromes ["" "a" "aba" "abba"])
+(def not-palindromes ["ab" "abax" "xabba"])
+
+(deftest test-palindrome?
+  (testing "Identifies palindrome"
+    (assert (every? palindrome? palindromes)))
+  (testing "Identifies non palindrome"
+    (assert (not-any? palindrome? not-palindromes))))
+
+(deftest test-get-pivot
+  (testing "Gets pivot"
+    (is "aa" (get-pivot "a", "a"))
+    (is "aa" (get-pivot "aa", "b"))
+    (is "a" (get-pivot "a" "b"))
+    (is "abba" (get-pivot "abba" nil))
+    (is "" (get-pivot "" "a"))))
+
+(deftest test-update-longest
+  (testing "Updates longest palindrome result set"
+    (is ["x"] (update-longest "x" []))
+    (is ["xy"] (update-longest "x" ["xy"]))
+    (is ["x" "y"] (update-longest "x" ["y"]))))
